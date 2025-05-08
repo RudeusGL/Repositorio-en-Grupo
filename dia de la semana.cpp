a12157dfe35ab735309fec24c4469f8deff32835
valeriaescobard <ve168745@gmail.com>
2025-05-08 09:46:01 -0600

    Add files via upload

---
+++ dia de la semana.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int dia;
+    cout << "¿Qué día es hoy? ";
+    cin >> dia;
+
+    switch (dia){
+        case 1:
+        cout << " Salir a caminar ";
+        
+        break;
+
+        case 2:
+        cout << " Ir al gym";
+       
+        break;
+
+        case 3:
+        cout << " Trabajar ";
+        
+        break;
+
+
+        case 4:
+        cout << " Correr ";
+        
+        break;
+
+
+        case 5:
+        cout << " Yoga ";
+        
+        break;
+
+
+        case 6:
+        cout << " Pilates";
+        
+        break;
+
+        case 7:
+        cout << " Ir a la u";
+      
+        break;
+
+    }
+
+  
+    }
+   
